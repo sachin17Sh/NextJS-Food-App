5378bdef0cb3b447690baadba5ff7d9c8bd97578
Sachin Sharma <ss0382245@gmail.com>
2025-05-02 17:23:29 +0530

    added preview to image picket and modified the code

--- components/meals/image-picker.jsx
+++ components/meals/image-picker.jsx
@@ -1,12 +1,50 @@
+'use client'
+
+import { useRef, useState } from "react";
+import Image from "next/image";
+
 import classes from "./image-picker.module.css"
 
+
 export default function ImagePicker({ label, name }) {
+    const [pickedImage, setPickedImage] = useState(null);
+    const inputRef = useRef();
+    function handlePickClick() {
+        inputRef.current.click()
+    }
+    function handleImageChange(event) {
+        const file = event.target.files[0];
+        if (!file) {
+            setPickedImage(null);
+            return;
+        }
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result)
+        }
+
+        fileReader.readAsDataURL(file);
+    }
     return (
         <div className={classes.picker}>
             <label htmlFor={name}> {label}</label>
             <div className={classes.controls}>
-                <input type="file" id={name} accept="image/png, image/jpeg" name={name} />
+                <div className={classes.preview}>
+                    {!pickedImage && <p>No image chosen</p>}
+                    {pickedImage && <Image src={pickedImage} alt="Image picket by user" fill />}
+                </div>
+                <input
+                    className={classes.input}
+                    type="file"
+                    id={name}
+                    accept="image/png, image/jpeg"
+                    name={name}
+                    ref={inputRef}
+                    onChange={handleImageChange}
+                    required
+                />
             </div>
+            <button className={classes.button} type="button" onClick={handlePickClick}>Pick An Image</button>
         </div>
     );
 }
